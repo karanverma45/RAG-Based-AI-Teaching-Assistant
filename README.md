daa1aa0d2d9e34d9c1923f69c530fb275cb35e45
Karan verma <karan279104@gmail.com>
2025-10-02 02:23:40 +0530

    Added new file and updated existing files

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # RAG-based-AI-Teaching-Assistant
 RAG Base Project – A work-in-progress project implementing Retrieval-Augmented Generation (RAG). It combines document retrieval with LLMs to deliver accurate, context-aware answers. Includes data ingestion, embeddings, retrieval pipeline, and a simple web interface.
+
+
+# How to use this RAG AI Teaching Assistant on your on data 
+## Step 1 - Collect your videos 
+Move all your video files to the videos folder 
+
+## Step 2 - Convert to mp3
+convert all the videos files to mp3 by running video_to_mp3
+
+## Step 3 - Convert mp3 to json 
+Convert all the mp3 files to json by running mp3_to_json
+
+## Step 4 - Convert the json files to vectors 
+Use the file preprocess_json to convert the json files to a dataframe with Embedding and save it a joblib pickle
+
+## step 5 - Prompt generation and feeding to LLM
+Read the joblib file and load it into the memory. Then create a relevent prompt as per the user query and feed it to the LLM
--- process_incoming.py
+++ process_incoming.py
@@ -1,7 +1,37 @@
-import numpy as np
-import pandas as pd
+import pandas as pd 
 from sklearn.metrics.pairwise import cosine_similarity
-from create_chunks import create_embedding
+import numpy as np 
+import joblib 
+import requests
+
+
+def create_embedding(text_list):
+    # https://github.com/ollama/ollama/blob/main/docs/api.md#generate-embeddings
+    r = requests.post("http://localhost:11434/api/embed", json={
+        "model": "bge-m3",
+        "input": text_list
+    })
+
+    embedding = r.json()["embeddings"] 
+    return embedding
+
+def inference(prompt):
+    r = requests.post("http://localhost:11434/api/generate", json={
+        #"model": "deepseek-r1",
+        "model": "llama3.2",
+        "prompt": prompt,
+        "stream": False
+    })
+    
+    response = r.json()
+    print(response)
+    return response
+
+
+
+df = joblib.load('embeddings.joblib')
+
+
 incoming_query = input("Ask a Question: ")
 question_embedding = create_embedding([incoming_query])[0] 
 
@@ -9,9 +39,28 @@ question_embedding = create_embedding([incoming_query])[0]
 # print(np.vstack(df['embedding'].values))
 # print(np.vstack(df['embedding']).shape)
 similarities = cosine_similarity(np.vstack(df['embedding']), [question_embedding]).flatten()
-print(similarities)
-top_results = 3
+# print(similarities)
+top_results = 5
 max_indx = similarities.argsort()[::-1][0:top_results]
-print(max_indx)
+# print(max_indx)
 new_df = df.loc[max_indx] 
-print(new_df[["title", "number", "text"]])
+# print(new_df[["title", "number", "text"]])
+prompt = f'''I am teaching web devlopment in my sigma web development course. Here are video subtitle chunk containing video title, video number, start time in seconds, end time in seconds, the text at that time:
+
+{new_df[["title" , "number", "start", "end", "text"]].to_json(orient="records")}
+
+-------------------------------------------------------------------------------------------------------------
+{incoming_query}
+User ask this question related to the video chunk, you have to answer in human way(dont mention the above format, its just for you) where is and how much content is taught in which video (in which and at what timestamp) and guide the user to go to that particular video. If user asks unrelated question, tell him that you can only answer question related to the course
+'''
+
+with open("prompt.txt", "w") as f:
+    f.write(prompt)
+
+response = inference(prompt)["response"]
+print(response)
+
+with open("response.txt", "w") as f:
+    f.write(response)
+# for index, item in new_df.iterrows():
+#     print(index, item["title"], item["number"], item["text"], item["start"], item["end"])
